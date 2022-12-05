d5f7b4b43d367975873337b19efd8f9375614c36
xaljavaxlv <xaljavamail@gmail.com>
2022-12-05 15:55:54 -0800

    Initial Commit

---
+++ SpaceX MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX MVVM
+//
+//  Created by Vlad Zavada on 12/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
